556e2cd3d2cf90e0c619104e5c22d4bc4258fa6f
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-11-16 10:48:31 -0500

    fix: stabilize storybook setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -9,6 +10,11 @@ const config: StorybookConfig = {
     },
     docs: {
         autodocs: 'tag',
+    },
+    async viteFinal(config) {
+        config.plugins = config.plugins ?? [];
+        config.plugins.push(tsconfigPaths());
+        return config;
     },
 };
 
